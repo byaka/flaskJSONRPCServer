d6c8575f9475c65cd8e60d642046225dc191560b
BYaka <byaka.life@gmail.com>
2015-06-21 14:50:15 +0300

    Version 0.4.1 New readme and some other changes

--- setup.py
+++ setup.py
@@ -8,14 +8,18 @@ setup(
    version           =__import__('flaskJSONRPCServer').__version__,
    packages          =find_packages(exclude=["tests.*", "tests"]),
    requires          =['python (>= 2.6)', 'flask (>= 0.10)'],
+   install_requires  =['flask>=0.10'],
+   extras_require    ={
+                        'gevent': ['gevent>=1.0']
+                     },
    description       ='A Python JSON-RPC over HTTP with flask and gevent',
-   long_description  =open('README.md').read(),
+   long_description  =open('README.rst').read(),
    author            ='Jhon Byaka',
    author_email      ='byaka.life@gmail.com',
    url               ='https://github.com/byaka/flaskJSONRPCServer',
    download_url      ='https://github.com/byaka/flaskJSONRPCServer/tarball/master',
-   license           ='Apache License 2.0',
-   keywords          ='flask json-rpc gevent',
+   license           ='Apache 2.0',
+   keywords          ='flask json-rpc jsonrpc gevent',
    classifiers       =[
       'Environment :: Web Environment',
       'Framework :: Flask',
